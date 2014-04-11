5692dafc6477d1f742d7b491d16ae4c5374473d3
Cindyalarcon <cindyjeanntte@yahoo.com>
2014-04-11 17:31:54 +0000

    We are making a paper, rock, scissors games.

--- simplerps.rb
+++ simplerps.rb
@@ -1,4 +1,9 @@
-OUTCOMES= {"rock"=>{"rock"=>"tie"=>"paper","player 2"=>"scissors"},
-           "paper"=>{},
-           "scissors"=>{}
-           }
+OUTCOMES = {"rock" => {"rock" => "tie", "paper" => "player 2", "scissors" => "player 1"},
+          "paper" => {"paper" => "tie", "rock" => "player 1", "scissors" => "player 2"},
+           "scissors" => {"scissors" => "tie", "rock" => "player 2", "paper" => "player 1"}}
+computer_choice = ["paper","rock","scissors"].sample 
+puts "Pick a choice Rock, Paper, Scissors?"
+user_choice = gets.chomp
+puts OUTCOMES[user_choice][computer_choice]
+          
+        
